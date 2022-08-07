2ec2ec1e0e8ece3b9faf870a6b73ba7aecbbd7da
aycish <aycish07@gmail.com>
2022-08-07 16:31:14 +0900

    feat : initialize project for spring project

---
+++ src/main/java/me/aycish/java8to11/Java8to11Application.java
@@ -0,0 +1,8 @@
+package me.aycish.java8to11;
+
+public class Java8to11Application {
+
+	public static void main(String[] args) {
+		System.out.println("Main application");
+	}
+}
---
+++ src/test/java/me/aycish/java8to11/Java8to11ApplicationTest.java
@@ -0,0 +1,4 @@
+package me.aycish.java8to11;
+
+public class Java8to11ApplicationTest {
+}
