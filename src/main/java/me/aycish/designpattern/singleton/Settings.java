ba314d8b8b78d1d281d01c6162065a3bb9adb6b8
aycish <aycish07@gmail.com>
2022-09-04 18:09:36 +0900

    feat : 멀티 스레딩 환경을 고려한 경우의 구현 방법 및 성능상의 이슈를 고려한 방법 추가

--- src/main/java/me/aycish/designpattern/singleton/Settings.java
+++ src/main/java/me/aycish/designpattern/singleton/Settings.java
@@ -2,9 +2,15 @@ package me.aycish.designpattern.singleton;
 
 public class Settings {
     private static Settings instance;
+    /* eager initialization : 가벼운 클래스의 경우, synchronized 없이 사용할 수 있는 방법 */
+    private static final Settings INSTANCE = new Settings();
+
+    /* double checked locking 방법 */
+    private static volatile Settings doubleCheckedInstance;
     private Settings() {}
 
-    public static Settings getInstance() {
+    /* 생성에 추가적인 작업이 많은 경우*/
+    public static synchronized Settings getInstance() {
         if (instance == null) {
             instance = new Settings();
         }
@@ -12,4 +18,30 @@ public class Settings {
         return instance;
     }
 
+    /* Eager initialization */
+    public static Settings getFinalInstance() {
+        return INSTANCE;
+    }
+
+    /* double checked locking 사용 */
+    public static Settings getDoubleCheckedLocking() {
+        if (doubleCheckedInstance == null) {
+            synchronized (Settings.class) {
+                if (doubleCheckedInstance == null) {
+                    doubleCheckedInstance = new Settings();
+                }
+            }
+        }
+
+        return doubleCheckedInstance;
+    }
+
+    /* static inner 클래스 사용 */
+    private static class SettingsHolder {
+        private static final Settings INNER_INSTANCE= new Settings();
+    }
+
+    public static  Settings getInnerClassInstance() {
+        return SettingsHolder.INNER_INSTANCE;
+    }
 }
